1ae60cea24bbb2ba7e4fda1b2ea2dce7a8b80430
DanielPaz19 <anyepaz1019@gmail.com>
2022-09-26 23:27:43 +0800

    initialization

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request) {
+       return User::create([
+            'username' => $request->input('username'),
+            'password' => Hash::make($request->input('password')),
+            'role' => $request->input('role'),
+        ]);
+
+    }
+
+    public function user() {
+        return 'Authenticated user';
+    }
+}
