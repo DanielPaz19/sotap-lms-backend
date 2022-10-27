d3cef6258fcd01253d1e6e541ee48f9edfe630d6
DanielPaz19 <anyepaz1019@gmail.com>
2022-10-28 03:52:11 +0800

    try the get method

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -79,14 +79,17 @@ class TeacherController extends Controller
 
     public function topics(Teacher $teacher, Request $request)
     {
-        // if ($request->has('subject')) {
-        return  $teacher->topics->where("subject_id", $request->subject);
-        // }
-        // if ($request->query()) {
+        if ($request->query()) {
+            if ($request->has('subject')) {
+                return response()
+                    ->json([
+                        "data" => $teacher->topics()->where("subject_id", $request->subject)->get()
+                    ]);
+            }
 
-        //     return response()->json(["message" => "Invalid Query Params"], Response::HTTP_BAD_REQUEST);
-        // }
+            return response()->json(["message" => "Invalid Query Params"], Response::HTTP_BAD_REQUEST);
+        }
 
-        // return response()->json(["data" => $teacher->topics]);
+        return response()->json(["data" => $teacher->topics]);
     }
 }
