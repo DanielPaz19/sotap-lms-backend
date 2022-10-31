39c7040d2a86022d990ebe36e920aaf363963459
DanielPaz19 <anyepaz1019@gmail.com>
2022-11-01 03:44:57 +0800

    subject resource add img_url

--- app/Http/Resources/Subject.php
+++ app/Http/Resources/Subject.php
@@ -21,7 +21,8 @@ class Subject extends JsonResource
             'subject_code' => $this->subject_code,
             'subject_name' => $this->subject_name,
             'subject_description' => $this->subject_description,
-            'teachers'=> $this->teachers,
+            'img_url' => $this->img_url,
+            'teachers' => $this->teachers,
         ];
     }
 }
